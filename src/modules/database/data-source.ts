ef676c811a0dce5da072afad24082e1d5e0a7c98
shahid <khanshahid9283@gmail.com>
2022-11-07 12:41:10 +0530

    nest-cli updated

--- src/modules/database/data-source.ts
+++ src/modules/database/data-source.ts
@@ -13,8 +13,8 @@ export const dataSourceOptions: DataSourceOptions = {
   database: process.env.DB_NAME,
   entities: [path.join(__dirname, '..', '**', '**', '*.entity{.ts,.js}')],
   migrations: [path.join(__dirname, 'migrations', '*{.ts,.js}')],
-  migrationsRun: process.env.NODE_ENV === 'development',
-  synchronize: process.env.NODE_ENV !== 'development',
+  migrationsRun: process.env.NODE_ENV !== 'development',
+  synchronize: process.env.NODE_ENV === 'development',
   logging: true,
   logger: 'file',
 };
